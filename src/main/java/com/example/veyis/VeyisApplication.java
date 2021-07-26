2afe16d20a163f7e7cccc3d6ca059af251460518
Veyis Turgut <vturgut68@gmail.com>
2021-07-26 17:49:12 +0300

    initialized spring application

---
+++ src/main/java/com/example/veyis/VeyisApplication.java
@@ -0,0 +1,13 @@
+package com.example.veyis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeyisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeyisApplication.class, args);
+	}
+
+}
